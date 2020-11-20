e791b9754c50ac8a7afbbd348a37da2bf33116bd
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2020-11-21 00:44:26 +0900

    Add: init project

---
+++ CtaReactiveMaster/CtaReactiveMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CtaReactiveMaster
+//
+//  Created by 小幡 十矛 on 2020/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
