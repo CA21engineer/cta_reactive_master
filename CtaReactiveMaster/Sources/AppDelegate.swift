0a1e6d773c0f2bd0047b6eed188f586f9db09f3c
Takuma Osada <judgment9ttk069@gmail.com>
2021-01-11 22:28:38 +0900

    hotfix uiwindow

--- CtaReactiveMaster/Sources/AppDelegate.swift
+++ CtaReactiveMaster/Sources/AppDelegate.swift
@@ -13,12 +13,8 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UINavigationController(rootViewController:  HomeViewController())
-        self.window = window
-        self.window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController:  HomeViewController())
+        window?.makeKeyAndVisible()
 
         return true
     }
